06f5d01cd2e0148053c37604ccdf13cbdb879967
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-11-09 21:55:48 -0800

    Add demo files

---
+++ demo/index.js
@@ -0,0 +1,11 @@
+const faker = require('faker')
+
+// const name = faker.name.findName()
+// const city = faker.address.city()
+// const product = faker.commerce.productName()
+const email = faker.internet.email()
+
+// console.log(name)
+// console.log(city)
+// console.log(product)
+console.log(email)
